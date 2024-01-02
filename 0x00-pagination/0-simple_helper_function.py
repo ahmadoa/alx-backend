6004a308082023a128bff70dc62d33bf7e5e5934
ahmadoa <ouladaouida@gmail.com>
2024-01-02 00:20:09 +0000

    helper function that returns tuple containing start index & end index

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""0. simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """returns a tuple of size two containing a start index
+    and an end index corresponding to the range of indexes"""
+    return ((page - 1) * page_size, page * page_size)
